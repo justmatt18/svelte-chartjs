4337c1b9a7647822a8f137c277103d76586991a9
justmatt18 <marcomatildo@gmail.com>
2019-08-15 17:25:11 +0800

    updated src and public files

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,53 @@
 <script>
-	export let name;
+	import Chart from './Chart.svelte'
+
+	let labels = ['Marco', 'James', 'Xavier', 'Pordz', 'Awjay', 'Barry Allen']
+	let data = [1,2,3,4,5,6]
+	let label = "# of Votes"
+
 </script>
 
-<style>
-	h1 {
-		color: purple;
-	}
-</style>
+<div class="input-score">
+	<div class="score">
+		<label for="marco">Marco</label>
+		<input type="number" bind:value={data[0]} name="marco">
+	</div>
+	<div class="score">
+		<label for="allen">James</label>
+		<input type="number" bind:value={data[1]} name="allen">
+	</div>
+	<div class="score">
+		<label for="xavier">Xavier</label>
+		<input type="number" bind:value={data[2]} name="xavier">
+	</div>
+	<div class="score">
+		<label for="awjay">Pordz</label>
+		<input type="number" bind:value={data[3]} name="awjay">
+	</div>
+	<div class="score">
+		<label for="fordi">Awjay</label>
+		<input type="number" bind:value={data[4]} name="fordi">
+	</div>
+	<div class="score">
+		<label for="jamezz">Barry Allen</label>
+		<input type="number" bind:value={data[5]} name="jamezz">
+	</div>
+</div>
 
-<h1>Hello {name}!</h1>
+
+<Chart data={data} labels={labels} label={label}/>
+
+
+<style>
+.input-score {
+	display: flex;
+	flex-direction: row;
+	gap: 5px;
+}
+.input-score > div
+{
+    flex:1 1 auto;
+    text-align:center;
+    margin:5px; 
+}
+</style>
---
+++ src/Chart.svelte
@@ -0,0 +1,62 @@
+<script>
+import {afterUpdate} from 'svelte';
+
+export let labels;
+export let label;
+export let data;
+
+var ctx;
+var myChart;
+
+
+function createChart() {
+    ctx = document.getElementById('myChart').getContext('2d');
+    if (myChart) myChart.destroy();
+    myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels,
+            datasets: [{
+                label,
+                data,
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
+        }
+    });
+}
+afterUpdate(
+    createChart
+)
+
+</script>
+
+<div class="container">
+    <canvas id="myChart" width="250" height="100"></canvas>
+</div>
+
+
